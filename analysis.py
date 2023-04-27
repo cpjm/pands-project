0a2795f57fe035647031fed57790f2c644881850
Ciaran Moran <ciaranmoran@gmail.com>
2023-04-27 17:30:46 +0100

    added df.describe and write to a text file

--- analysis.py
+++ analysis.py
@@ -11,5 +11,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#ref https://www.w3schools.com/python/pandas/pandas_csv.asp#gsc.tab=0
 df = pd.read_csv('data/iris.data')
 print(df.to_string()) 
+
+#ref https://www.w3schools.com/python/pandas/ref_df_describe.asp
+print(df.describe())
+
+#Generate the 
+#ref https://pandashowto.com/how-to-save-dataframe-as-text-file/
+#df.to_string("myfile.txt")
+df.describe().to_string("iris_summary.txt")
+
